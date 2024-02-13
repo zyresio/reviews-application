96d417b14750d95a2cc015025a6b91c9dcbd69b2
Leon Benz <l.benz@zyres.com>
2024-02-13 16:09:58 +0100

    init

---
+++ custom-application-config.mjs
@@ -0,0 +1,39 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomApplication}
+ */
+const config = {
+  name: 'Reviews',
+  entryPointUriPath,
+  cloudIdentifier: 'gcp-eu',
+  env: {
+    development: {
+      initialProjectKey: 'zyrestestpro01',
+    },
+    production: {
+      applicationId: 'TODO',
+      url: 'https://your_app_hostname.com',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Reviews',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+  submenuLinks: [
+    {
+      uriPath: 'approval',
+      defaultLabel: 'Approval',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+// Make sure to import the helper functions from the `ssr` entry point.
+import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
+
+export const entryPointUriPath = 'reviews';
+
+export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
