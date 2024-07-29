44c58984dffcb48b5cf53580cc50c2fcd958b4f1
Leon Benz <l.benz@zyres.com>
2024-07-29 10:48:57 +0200

    use env for initial project key

--- review-mc-app/custom-application-config.mjs
+++ review-mc-app/custom-application-config.mjs
@@ -9,7 +9,7 @@ const config = {
   cloudIdentifier: '${env:CLOUD_IDENTIFIER}',
   env: {
     development: {
-      initialProjectKey: 'zyrestestpro01',
+      initialProjectKey: process.env.CTP_INITIAL_PROJECT_KEY ?? 'fallback',
     },
     production: {
       applicationId: '${env:CUSTOM_APPLICATION_ID}',
